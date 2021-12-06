ea755e2582535e30f3b5ce1f648e5ab00466582a
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-12-06 18:38:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -17,19 +17,19 @@ def save_file(infile):
 print(os.getenv("USERNAME"))
 
 # pull secrets from a non-tracked secrets file
-con = Minio(
-    "http://172.31.2.211:38687/",
-    access_key=os.getenv("USERNAME"),
-    secret_key=os.getenv("PASSWORD"),
-    secure=True,
-)
+# con = Minio(
+#     "http://172.31.2.211:38687/",
+#     access_key=os.getenv("USERNAME"),
+#     secret_key=os.getenv("PASSWORD"),
+#     secure=True,
+# )
 
-input_file = st.file_uploader("Drag your input file here")
+# input_file = st.file_uploader("Drag your input file here")
 
-if input_file is not None:
-  name_file = save_file(input_file)
+# if input_file is not None:
+#   name_file = save_file(input_file)
 
-# Upload file as object name
-if name_file:
+# # Upload file as object name
+# if name_file:
 
-  con.fput_object(BUCKET_NAME,"testupload.txt" , name_file)
+#   con.fput_object(BUCKET_NAME,"testupload.txt" , name_file)
